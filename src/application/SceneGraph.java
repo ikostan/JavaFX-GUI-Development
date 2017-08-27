283fb42255b351318b3549b9c817431d2b650169
ikostan <superegorius@gmail.com>
2017-08-26 18:40:24 -0700

    SceneGraph >>> done

--- src/application/SceneGraph.java
+++ src/application/SceneGraph.java
@@ -9,9 +9,15 @@ import javafx.stage.Stage;
 
 public class SceneGraph extends Application{
 
+	private final static String TAG = SceneGraph.class.getName();
+	private final static String mNAME = "Method name: ";
+	private final String TITLE = "JavaFX: Scene Graph Example";
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
+		printLog("start", "started");
+		
 		StackPane root = new StackPane();
 		
 		//Add a leaf node
@@ -21,14 +27,20 @@ public class SceneGraph extends Application{
 		
 		Scene scene = new Scene(root, 400, 400);
 		
-		primaryStage.setTitle("Hello World (JavaFX: Scene Graph Example)");
+		primaryStage.setTitle(TITLE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
 	public static void main(String[] args){
 		
+		printLog("main", "started");
 		launch(args);
+	}
+	
+	private static void printLog(String method, String action){
+		
+		System.out.println(TAG + " >>> " + mNAME + method + " >>> " + action);
 	}
 
 }
