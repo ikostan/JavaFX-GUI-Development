7aae9f1225fba9b765dc30f25ad73f71896e6428
ikostan <superegorius@gmail.com>
2017-08-27 15:19:19 -0700

    Comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,19 +15,23 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
-	private final static String TAG = Main.class.getName().replaceAll("application.", "");
-	
-	private BorderPane pane;
+	//Final parameters
+	private final static String TAG = Main.class.getName().replaceAll("application.", "");	
 	private final String btnName = "LAUNCH";
+	private final String JAR = ".jar";
+	private final String exeFolder = "\\src\\executable";
+
+	//GUI objects
+	private Scene scene;
+	private BorderPane pane;
 	private Button btnLaunch;
 	private ComboBox combo;
-	private Scene scene;
+	
+	//Run time parameters
 	private File[] fileArray;
-	File rootFolder;
+	private File rootFolder;
 	private String rootFolderPath;
-	private final String exeFolder = "\\src\\executable";
 	private Path path;
-	private final String JAR = ".jar";
 	
 	
 	//Main method
@@ -51,7 +54,7 @@ public class Main extends Application{
 		setStage(mainStage);
 	}
 	
-	
+	//Set 'Launch' button
 	private void setBtn(){
 		
 		System.out.println(TAG + ": setBtn method called");		
@@ -72,15 +75,13 @@ public class Main extends Application{
 					 
 					String error = "The list is empty. There is nothing to show.";
 					System.out.println(TAG + ": " + error);		
-
-				}
-				
+				}				
 			}
 		});	
 		pane.setBottom(btnLaunch);
 	}
 
-	
+	//Set main stage
 	private void setStage(Stage mainStage){
 		
 		System.out.println(TAG + ": setStage method called");		
@@ -90,12 +91,12 @@ public class Main extends Application{
 		mainStage.show();
 	}
 	
-	
+	//Set combo box
 	private void setCombo(){
 		
 		System.out.println(TAG + ": setCombo method called");		
 		
-		combo = new ComboBox();
+		combo = new ComboBox(); //Create a new Combo Box
 		scanDirectory();
 		
 		if(fileArray.length > 0){
@@ -106,35 +107,36 @@ public class Main extends Application{
 				
 				if(file.toString().contains(JAR)){
 					
-					names.add(file.getName());
+					names.add(file.getName()); //Add JAR file to the list
+					System.out.println(TAG + " new JAR file found: " + file.getName());		
 				}
 			}
 			
-			combo.getItems().addAll(names);
+			combo.getItems().addAll(names); //Add all JAR files to the combo list
 			combo.getSelectionModel().selectFirst(); //Show first item by default
 		}
 		
 		pane.setCenter(combo);
 	}
 	
-	
+	//Get project folder
 	private void getRootFolder(){
 			
 		System.out.println(TAG + ": setAbsPath method called");		
-		rootFolderPath = new File("").getAbsolutePath();	
+		rootFolderPath = new File("").getAbsolutePath(); //Get absolute project path	
 		System.out.println(TAG + " rootFolderPath: " + rootFolderPath);		
 	}
 	
-	
+	//Scan project directory ("executable" folder) 
 	private void scanDirectory(){
 		
 		System.out.println(TAG + ": scanDirectory method called");		
 		
 		getRootFolder();
-		String fullPath = rootFolderPath + exeFolder;
+		String fullPath = rootFolderPath + exeFolder; //Set full path for "executable" folder
 		System.out.println(TAG + " fullPath: "  + fullPath);		
-		rootFolder = new File(fullPath);		
-		fileArray = rootFolder.listFiles();
+		rootFolder = new File(fullPath); //Set "executable" folder		
+		fileArray = rootFolder.listFiles(); //Get all files from "executable" folder
 		System.out.println(TAG + ": " + fileArray.length + " files found");		
 	}
 	
