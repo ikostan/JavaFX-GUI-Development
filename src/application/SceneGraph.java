d31a91203142bf93cd9ce5f1c9b3de2c1b5abcbc
ikostan <superegorius@gmail.com>
2017-08-26 18:30:18 -0700

    First run

---
+++ src/application/SceneGraph.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class SceneGraph extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+		StackPane root = new StackPane();
+		
+		//Add a leaf node
+		Circle cir = new Circle(200,200,100);
+		cir.setFill(Color.CORAL);
+		root.getChildren().add(cir);
+		
+		Scene scene = new Scene(root, 400, 400);
+		
+		primaryStage.setTitle("Hello World (JavaFX: Scene Graph Example)");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+
+		
+		launch(args);
+	}
+
+}
