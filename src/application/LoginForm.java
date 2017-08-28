4f2f569e96799b51c34898fa0b114d1dcde7dfe2
ikostan <superegorius@gmail.com>
2017-08-27 18:37:49 -0700

    LoginForm

---
+++ src/application/LoginForm.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginForm extends Application{
+
+	public static void main(String[] args) {
+		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		
+		primaryStage.show();
+	}
+
+}
